d4d4d6ac25b253442b3015e7a5280cf3270b5103
tyoung12290 <tyler.young@wwt.com>
2018-06-21 18:11:09 -0500

    retrieving and loading previous days data

--- Documents/DailyData/DailyData.py
+++ Documents/DailyData/DailyData.py
@@ -1,59 +1,40 @@
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
+import pandas as pd
+import psycopg2
 from pandas.io.html import read_html
 from pandas import DataFrame
 from bs4 import BeautifulSoup
 import platform
 import time
-from HTMLTableParser import HTMLTableParser
 from sqlalchemy import create_engine
+import time
 
-if platform.system() == 'Windows':
-    PHANTOMJS_PATH = './phantomjs.exe'
-else:
-    PHANTOMJS_PATH = './phantomjs'
-
-username = 'tyoung12290@gmail.com'
-passord = 'Tjytjy90'
-#    "cc_session_id": "<TOKEN>"
-
+username = 'tyoung1229'
+password = 'Tjytjy90'
+ts = time.time()*1000
 stats = DataFrame()
+baseUrl = "http://mlb.mlb.com/stats/sortable.jsp#elem=%5Bobject+Object%5D&tab_level=child&click_text=Sortable+Player+hitting&game_type='R'&season=2018&season_type=ANY&league_code='MLB'&sectionType=sp&statType=hitting&"
+variableUrl = "page=All&ts={}&timeframe=d1&split=&last_x_days=1".format(ts)
+Url = baseUrl + variableUrl
+print(Url)
+#"https://www.baseball-reference.com/leagues/MLB/2017-standard-batting.shtml"
 
-Url = "https://www.baseball-reference.com/leagues/MLB/2017-standard-batting.shtml"
-
-driver = webdriver.PhantomJS(PHANTOMJS_PATH)
+driver = webdriver.PhantomJS()
 driver.set_window_size(1024, 768) # optional
 driver.get(Url)
 time.sleep(1)
-stats_list = pandas.read_html(driver.page_source,attrs={'id': 'players_standard_batting'} )
+stats_list = pd.read_html(driver.page_source,attrs={'class': 'stats_table'} )
+#stats_list = pd.read_html(driver.page_source,attrs={'id': 'players_standard_batting'} )
 print("html to table")
+print(stats_list)
 stats = stats_list[0]
-stats.drop(stats.columns[[0,29]],inplace=True,axis=1)
-stats.columns=['player','age','team','LG','G','PA','AB','R','H','2B','3B',
-				'HR','RBI','SB','CS','BB','SO','BA','OBP','SLG',
-				'OPS','OPS+','TB','GDP','HBP','SH','SF','IBB']
-engine= create_engine('postgresql://postgres:postgres123@localhost:5432/espn_data')
+cols = [0,18,19,20,21,22,23,24,25,26,27,28,29,30,31]
+stats.drop(stats.columns[[cols]],inplace=True,axis=1)
+print(stats)
+stats.columns=['Player','Team','Pos','G','AB','R','H','2B','3B','HR','RBI','BB',
+				'SO','SB','CS','AVG','OBP']
 
-stats.to_sql("player_data",engine,if_exists='replace')
-print("table to database")
-#soup = BeautifulSoup(driver.page_source,"html.parser")
-#hp = HTMLTableParser()
-#table = hp.parse_url(soup)[1][1]
-#table.head()
-#games = soup.find_all('meta')
+engine = create_engine('postgresql://tyoung12290:Tjytjy90@mlb.ck81qylct4si.us-east-2.rds.amazonaws.com:5432/MLB')
 
-#table = driver.find_element_by_xpath('//*[@id = "players_standard_batting"]')
-#table_html = table.get_attribute('innerHTML')
-#stats = read_html(table_html)[0]
-
-#table = driver.find_elements_by_xpath('//*[@id = "players_standard_batting"]')
-#for row in table.find_elements_by_xpath(".//tr"):
-#	cell = row.find_elements_by_tag_name("td")[1]
-#print(cell.text)
-
-#driver.find_element_by_xpath('.//*[@name="email"]').send_keys('tyoung12290@gmail.com')
-#driver.find_element_by_xpath('.//*[@name="password"]').send_keys('Tjytjy90')
-#driver.find_element_by_xpath('.//*[@name="login"]').click()
-#driver.get("https://www.fanduel.com/games")
-#item=driver.find_element_by_class_name('sport-toggle-menu__item')
-#print(item.text,item.tag_name)
+stats.to_sql("player_game_data",engine,if_exists='replace')
+#print("table to database")
